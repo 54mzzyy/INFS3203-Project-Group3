0ad5edea2ca06876a1b807b72d6f19d6be93ee62
Ahmed Hussein <138488021+Ah87979@users.noreply.github.com>
2024-03-19 17:46:10 +0300

    Update persistence.js

--- persistence.js
+++ persistence.js
@@ -9,30 +9,6 @@ async function connectDatabase() {
     }
 }
 
-async function startSession(session_data) {
-    await connectDatabase()
-    let db = client.db('project')
-    let sessions = db.collection('sessions')
-    sessions.insertOne(session_data)
-}
-
-async function getSession(session_id) {
-    await connectDatabase()
-    let db = client.db('project')
-    let sessions = db.collection('sessions')
-    let result = await sessions.find({sessionNumber: session_id})
-    let resultData = await result.toArray()
-    return resultData[0]
-}
-
-async function deleteSession(session_id) {
-    await connectDatabase()
-    let db = client.db('project')
-    let sessions = db.collection('sessions')
-    let result = await sessions.find({sessionNumber: session_id})
-    delete result
-}
-
 module.exports = {
-    startSession, getSession, deleteSession
+    
 }
