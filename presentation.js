020ae57b7433905bb91178c233e2dc33c35cfb11
Ahmed Hussein <138488021+Ah87979@users.noreply.github.com>
2024-03-19 18:47:35 +0300

    Update presentation.js

--- presentation.js
+++ presentation.js
@@ -1,1 +1,13 @@
+const business = require('./business')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const handlebars = require('express-handlebars')
+let app = express()
 
+app.use(bodyParser.urlencoded())
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  
+})
