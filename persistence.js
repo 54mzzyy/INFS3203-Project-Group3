130fc2e9ad8021c5c6e440cef5dd3cfb26d5097c
Ahmed Hussein <138488021+Ah87979@users.noreply.github.com>
2024-03-19 18:49:19 +0300

    Update persistence.js

--- persistence.js
+++ persistence.js
@@ -1,11 +1,14 @@
 const { MongoClient } = require('mongodb')
 
-let client = undefined 
+let client = undefined
+let db = undefined
 
 async function connectDatabase() {
     if (!client) {
         client = new MongoClient('mongodb+srv://ahmed:12class34@cluster0.qgtdkrd.mongodb.net/')
         await client.connect()
+        db = client.db('tourq')
+        return db
     }
 }
 
