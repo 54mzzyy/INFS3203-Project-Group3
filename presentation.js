8a927bc793ab2bff98635ecfa12d91999d37d174
YKBGAMER <87877202+YKBGAMER@users.noreply.github.com>
2024-03-20 10:59:50 +0300

    Update presentation.js

--- presentation.js
+++ presentation.js
@@ -2,12 +2,43 @@ const business = require('./business')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const handlebars = require('express-handlebars')
 let app = express()
 
 app.use(bodyParser.urlencoded())
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
+  let username = req.body.username
+  let password = req.body.password
+  let verifiedUser = business.verifiedUser(username, password)
+  if(verifiedUser) {
+    res.redirect('/main')
+  }
+  else {
+    res.render('invalid')
+  }
+})
+
+app.post('/', (req, res) => {
+  
+})
+
+app.get('/main', (req, res) => {
+  
+})
+
+app.get('/map', (req, res) => {
+  
+})
+
+app.get('/map/:option', (req, res) => {
+  
+})
+
+app.get('/checklist', (req, res) => {
+  
+})
+
+app.get('/settings', (req, res) => {
   
 })
