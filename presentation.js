695cfe0cd86930d1ddceb75eca39a6a72d56521d
BibiAasiyaa <158258718+BibiAasiyaa@users.noreply.github.com>
2024-03-26 11:07:41 +0300

    handlebars package added

--- presentation.js
+++ presentation.js
@@ -2,6 +2,7 @@ const business = require('./business')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const handlebars = require('express-handlebars')
 let app = express()
 
 app.use(bodyParser.urlencoded())
