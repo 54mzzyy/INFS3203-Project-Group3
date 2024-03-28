73b84aa8f0435835928b831ad6134bb8de16ace5
Ahmed Hussein <138488021+Ah87979@users.noreply.github.com>
2024-03-28 11:33:24 +0300

    Major update

--- business.js
+++ business.js
@@ -1,15 +1,14 @@
 const persistence = require('./persistence')
-const { createHash, randomUUID } = require("node:crypto") #weCanRemoveThatLine
+const { createHash, randomUUID } = require("node:crypto")
 
 async function verifiedUser(u, p) {
   let details = await persistence.getUserDetails(u);
-  let hashedPass = createHash("sha512").update(p).digest("hex")  #weCanRemoveThatLine
+  let hashedPass = createHash("sha512").update(p).digest("hex")
   if (details == undefined || details.password != hashedPass) {
     return undefined;
   }
 }
 
 module.exports = {
-  attemptLogin
 }
 
